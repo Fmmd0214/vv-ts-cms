e02417cd45324eaa0441c50b34f0723a5ce035ec
Youli <ColaWyc@163.com>
2023-03-31 00:20:37 +0800

    vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
+import "normalize.css";
+import "./assets/css/index.less";
 import App from "./App.vue"; // 相当于any类型，不知道App是个组件,只是可以导入
+import router from "./router";
 
-import "./assets/main.css";
-
-createApp(App).mount("#app");
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,9 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+
+const router = createRouter({
+  history: createWebHashHistory(), // 哈希类型
+  // 映射关系：path => component 路径和组件
+  routes: [],
+});
+
+export default router;
