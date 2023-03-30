3de33de298673ff4f8cbac820cc1da8f65d162a2
Youli <ColaWyc@163.com>
2023-03-30 22:09:49 +0800

    删无用组件

--- env.d.ts
+++ env.d.ts
@@ -3,11 +3,12 @@
 
 // 这里让.vue文件生效
 declare module "*.vue" {
-    /**
-     * DefineComponent就是defineComponent的返回类型
-     * 所以可以直接用DefineComponent来定义vue组件
-     */
-    import { DefineComponent } from 'vue'
-    const component:DefineComponent // 这样引入.vue文件，就不是any类型了，而是一个真正的组件
-    export default component
+  /**
+   * DefineComponent就是defineComponent的返回类型
+   *
+   * 可以直接用DefineComponent来定义vue组件
+   */
+  import { DefineComponent } from "vue";
+  const component: DefineComponent; // 这样引入.vue文件，就不是any类型了，而是一个真正的组件
+  export default component;
 }
